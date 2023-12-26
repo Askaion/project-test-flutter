55e85ca1dd6af5dde22bc0af00979d8ca5bf7332
Katkatarr <melodyofagatha@gmail.com>
2023-12-26 12:59:47 +0700

    initial commit

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,59 +1,169 @@
 import 'package:flutter/material.dart';
-import 'second_screen.dart';
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController sentenceController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('First Screen')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(controller: nameController, decoration: InputDecoration(labelText: 'Name')),
-            TextField(controller: sentenceController, decoration: InputDecoration(labelText: 'Sentence')),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                bool isPalindrome = checkPalindrome(sentenceController.text);
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('isPalindrome'),
-                      content: Text(isPalindrome ? 'Palindrome' : 'Not Palindrome'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Text('OK'),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-              child: Text('Check'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/img/background.png'),
+                fit: BoxFit.cover,
+              ),
             ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SecondScreen(name: nameController.text),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 116, // Adjusted width for the profile image
+                    height: 116, // Adjusted height for the profile image
+                    margin: EdgeInsets.only(
+                        bottom: 56.0), // Added margin for the gap
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image:
+                            AssetImage('assets/img/profile.png'), // Image path
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                );
-              },
-              child: Text('Next'),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 39.88,
+                    padding: EdgeInsets.fromLTRB(16, 16, 16, 20),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color(0xFFFFFFFF),
+                          Color(0xFFFFFFFF),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(width: 0.5),
+                    ),
+                    child: TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Name',
+                        border: InputBorder.none,
+                        alignLabelWithHint: true,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 39.88,
+                    margin: EdgeInsets.only(top: 7.12),
+                    padding: EdgeInsets.fromLTRB(16, 16, 16, 20),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color(0xFFFFFFFF),
+                          Color(0xFFFFFFFF),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(width: 0.5),
+                    ),
+                    child: TextField(
+                      controller: sentenceController,
+                      decoration: InputDecoration(
+                        labelText: 'Sentence',
+                        border: InputBorder.none,
+                        alignLabelWithHint: true,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40), // Adjust the spacing here
+                  ElevatedButton(
+                    onPressed: () {
+                      bool isPalindrome =
+                          checkPalindrome(sentenceController.text);
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('isPalindrome'),
+                            content: Text(
+                                isPalindrome ? 'Palindrome' : 'Not Palindrome'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF2B637B), // Background color
+                      minimumSize: Size(
+                        MediaQuery.of(context).size.width - 32,
+                        41, // Adjusted height for the button
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: Text(
+                      'Check',
+                      style: TextStyle(
+                        color: Color(0xFFFFFFFF),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(
+                        context,
+                        '/second_screen',
+                        arguments: {'name': nameController.text},
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF2B637B), // Background color
+                      minimumSize: Size(
+                        MediaQuery.of(context).size.width - 32,
+                        41, // Adjusted height for the button
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: Text(
+                      'Next',
+                      style: TextStyle(
+                        color: Color(0xFFFFFFFF),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
